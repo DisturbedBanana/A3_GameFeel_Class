02e1445cb7a544be4b0f3ecda80d81832b7e3b95
DisturbedBanana <90250325+DisturbedBanana@users.noreply.github.com>
2023-11-07 09:09:50 +0100

    Update KenneyStateTurnBackDecelerate.cs

--- Assets/_LOK/Common/Characters/Kenney/Runtime/Scripts/States/KenneyStateTurnBackDecelerate.cs
+++ Assets/_LOK/Common/Characters/Kenney/Runtime/Scripts/States/KenneyStateTurnBackDecelerate.cs
@@ -33,31 +33,44 @@ namespace LOK.Common.Characters.Kenney
 
             _lockedReader = StateMachine.GetComponent<IMove2DLockedReader>();
             _dirReader = StateMachine.GetComponent<IMove2DDirReader>();
+            _speedMaxReader = StateMachine.GetComponent<IMove2DSpeedMaxReader>();
+            _orientReader = StateMachine.GetComponent<IMove2DOrientReader>();
+            _speedWriter = StateMachine.GetComponent<IMove2DSpeedWriter>();
+            _turnBackWriter = StateMachine.GetComponent<IMove2DTurnBackWriter>();
 
             if (!_lockedReader.AreMovementsLocked)
             {
-
+                _turnBackWriter.IsTurningBack = true;
+                _speedWriter.MoveSpeed = _speedMaxReader.MoveSpeedMax;
             }
         }
 
         protected override void OnStateEnter(AKenneyState previousState)
         {
             //Set IsTurningBack to true
+            _turnBackWriter.IsTurningBack = true;
             
             //Calculate _timer according to MoveSpeed / MoveSpeedMax / MovementsData.TurnBackDecelerationDuration
+            _timer = _speedWriter.MoveSpeed / _speedMaxReader.MoveSpeedMax * MovementsData.TurnBackDecelerationDuration;
         }
 
         protected override void OnStateExit(AKenneyState nextState)
         {
             //Set IsTurningBack to false
+            _turnBackWriter.IsTurningBack = false;
         }
         
         protected override void OnStateUpdate()
         {
             //Go to State Idle if Movements are locked
+            if (_lockedReader.AreMovementsLocked)
+            {
+                ChangeState(StateMachine.StateIdle);
+            }
 
             //If there is MoveDir and the angle between MoveDir and OrientDir > MovementsData.TurnBackAngleThreshold
                 //Go to StateAccelerate
+
 
             //Increment _timer with deltaTime
 
